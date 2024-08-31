5c4838d2616f2f4e5bb29268fce866cda25d25c6
Gabriel Resende <95802640+Boemia@users.noreply.github.com>
2024-08-31 13:20:21 -0300

    Criando um Sistema Bancário com Python
    Criando um Sistema Bancário com Python - NTT DATA - Engenharia de Dados com Python

---
+++ Desafios/Sistema-Bancário.py
@@ -0,0 +1,59 @@
+menu ="""
+
+[d] Depositar
+[s] Sacar
+[e] Extrato
+[q] Sair
+
+=>"""
+
+saldo = 0
+limite =500
+extrato = ""
+numero_saques = 0
+LIMITE_SAQUES = 3
+
+while True:
+    opcao = input(menu)
+
+    if opcao == "d":
+        valor = float(input("Informe o valor do depósito: "))
+
+        if valor > 0:
+            saldo += valor
+            extrato += f"Depósito: R$ {valor:.2f}\n"
+        else:
+            print("Operação falhou, valor informado inválido.")
+
+    elif opcao == "s":
+        valor = float(input("Informe o valor do saque: "))
+        excedeu_saldo = valor > saldo
+        excedeu_limite = valor > limite
+        excedeu_saques = numero_saques >= LIMITE_SAQUES
+
+        if excedeu_saldo:
+            print("Operação falhou, sem saldo suficiente.")
+
+        elif excedeu_limite:
+            print("Operação falhou, valor excedeu o limite.")
+
+        elif excedeu_saques:
+            print("Operação falhou, número de saques excedido.")
+
+        elif valor > 0:
+            saldo =- valor
+            extrato += f"Saque: R$ {valor:.2f}\n"
+
+        else:
+            print("Operação falhou, valor infirmado inválido.")
+
+    elif opcao == "e":
+        print("\n EXTRATO")
+        print("Não foram realizadas movimentações."if not extrato else extrato)
+        print(f"\nSaldo: R$ {saldo:.2f}")
+
+    elif opcao == "q":
+        break
+
+    else:
+        print("Operação inválida, selecione outra opção.")
