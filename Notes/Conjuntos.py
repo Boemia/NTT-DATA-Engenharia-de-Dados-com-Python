5feb34e049d88b461bffb445f8f10da026fa98ca
Gabriel Resende <95802640+Boemia@users.noreply.github.com>
2024-09-01 15:32:45 -0300

    update

---
+++ Notes/Conjuntos.py
@@ -0,0 +1,4 @@
+#Criando sets
+#set não possui objetos repetidos
+print(set([1, 2, 3, 2, 3, 4]))
+print(set("abacaxi"))
---
+++ Notes/Tuplas.py
@@ -0,0 +1,12 @@
+#tuplas são imutaveis
+
+frutas = ("laranja", "pera", "uva",)
+
+letras = tuple("python")
+
+numeros = tuple([1, 2, 3, 4])
+
+pais = ("Brasil",)
+
+frutas2 = ("laranja", "pera", "uva", "pera",)
+print(frutas2[2])
